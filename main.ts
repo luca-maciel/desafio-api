0be8f99cc740fe125de14599521ffd1cc564fc23
Lucas Maciel <lalucasforte56@gmail.com>
2026-08-04 12:37:12 -0300

    first commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+
+const express:any = require('express');
+const app:any = express();
+const PORT:number = 8081;
+
+app.use("/", (req:any, res:any)=>{
+    res.send("ta funfano");
+})
+
+
+app.listen(PORT, ()=>{
+    console.log("Servidor rodando em http://localhost:8081");
+});
+
